07ba771c0d1f44859d3b47ca0d5b8ba3c24b8452
Guiyun <i@guiyunweb.com>
2020-07-02 15:00:30 +0800

    feat:添加布局

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LayoutDefaultComponent} from './default/default.component';
 import {RoutesRoutingModule} from '../routes/routes-routing.module';
+import { HeaderComponent } from './default/header/header.component';
+import { FooterComponent } from './default/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    LayoutDefaultComponent
+    LayoutDefaultComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule,
